1f8d50232e093204dbc67becde7e3b7a31379e98
Ishaqq <ishaq8283@gmail.com>
2017-10-24 12:08:09 +0500

    Add files via upload

---
+++ js/diwali-event.js
@@ -0,0 +1,324 @@
+
+myObj2={
+  "code" : "0",
+  "msg" : "操作成功",
+  "data" : [ {
+    "nickname" : "Divya Bhardwaj",
+    "headimg" : "1504019782763.343502209436533",
+    "targetid" : 2206693,
+    "coins" : 8533500
+  }, {
+    "nickname" : "Jass Malik",
+    "headimg" : "1507192296990.jpg",
+    "targetid" : 2221523,
+    "coins" : 6603000
+  }, {
+    "nickname" : "\uD83C\uDF39H00ria \uD83C\uDD70ngel\uD83C\uDF39\uD83D\uDC7C",
+    "headimg" : "1508016801317.jpg",
+    "targetid" : 2039680,
+    "coins" : 5918250
+  }, {
+    "nickname" : "\uD83D\uDC98EmO❣Ali\uD83D\uDCA5Pardesi\uD83C\uDF1F",
+    "headimg" : "1508023014313.jpg",
+    "targetid" : 2097881,
+    "coins" : 3590250
+  }, {
+    "nickname" : "Irru aani malik",
+    "headimg" : "1507624032858.jpg",
+    "targetid" : 2144697,
+    "coins" : 2788250
+  }, {
+    "nickname" : "\uD83D\uDC98Lcky❣Sam\uD83D\uDCA5Pardsi\uD83C\uDF1F",
+    "headimg" : "1508138070746.jpg",
+    "targetid" : 2109908,
+    "coins" : 1645250
+  }, {
+    "nickname" : "\uD83D\uDC98DilJale\uD83D\uDCA5Pardesi\uD83C\uDF1F",
+    "headimg" : "1508010419016.jpg",
+    "targetid" : 2052696,
+    "coins" : 1347750
+  }, {
+    "nickname" : "Muskan Malik",
+    "headimg" : "1508164868432.jpg",
+    "targetid" : 2002210,
+    "coins" : 1156250
+  }, {
+    "nickname" : "RJ Asiya Malik",
+    "headimg" : "1507043064510.jpg",
+    "targetid" : 2238958,
+    "coins" : 1072750
+  }, {
+    "nickname" : "Meerab Awan",
+    "headimg" : "1507845087335.jpg",
+    "targetid" : 2237296,
+    "coins" : 995750
+  }, {
+    "nickname" : "❤Hafiza\uD83D\uDE0DRani\uD83D\uDC70\uD83D\uDC4C",
+    "headimg" : "1508110840174.jpg",
+    "targetid" : 2243906,
+    "coins" : 989750
+  }, {
+    "nickname" : "Vj\uD83D\uDC98S\uD83C\uDD70N\uD83C\uDD70\uD83D\uDE09 Kh\uD83C\uDD70N",
+    "headimg" : "1508156934506.jpg",
+    "targetid" : 2189073,
+    "coins" : 988000
+  }, {
+    "nickname" : "Mano bilee",
+    "headimg" : "1506252682002.jpg",
+    "targetid" : 2261832,
+    "coins" : 803250
+  }, {
+    "nickname" : "Vj DollMahi#\uD83D\uDC69\uD83C\uDFFC‍\uD83D\uDCBB",
+    "headimg" : "1508178602035.jpg",
+    "targetid" : 2198816,
+    "coins" : 717500
+  }, {
+    "nickname" : "\uD83D\uDE0DOUNAIS\uD83D\uDC9EKHAN \uD83D\uDE18",
+    "headimg" : "1508202262703.jpg",
+    "targetid" : 2143560,
+    "coins" : 694000
+  } ]
+}
+
+
+myObj3={
+  "code" : "0",
+  "msg" : "操作成功",
+  "data" : [ {
+    "nickname" : "\uD83C\uDF1FPARDESI\uD83C\uDF1FZAFAR\uD83C\uDF1F",
+    "headimg" : "1506435072853.jpg",
+    "userid" : 2207843,
+    "coins" : 35569500
+  }, {
+    "nickname" : "DON00007\uD83C\uDDEE\uD83C\uDDF3 pardesi",
+    "headimg" : "1507265755478.jpg",
+    "userid" : 2147833,
+    "coins" : 12091500
+  }, {
+    "nickname" : "\uD83D\uDEE9Pilot\uD83D\uDEE9",
+    "headimg" : "1504595349261.jpg",
+    "userid" : 2215552,
+    "coins" : 7834500
+  }, {
+    "nickname" : "\uD83D\uDC98EmO❣Ali\uD83D\uDCA5Pardesi\uD83C\uDF1F",
+    "headimg" : "1508023014313.jpg",
+    "userid" : 2097881,
+    "coins" : 4121500
+  }, {
+    "nickname" : "\uD83D\uDE1CBe_Parwah\uD83D\uDE1C",
+    "headimg" : "1507979483818.jpg",
+    "userid" : 2234413,
+    "coins" : 3259500
+  }, {
+    "nickname" : "GHAREEB THARKI \uD83D\uDC2F",
+    "headimg" : "1508186617427.jpg",
+    "userid" : 2207340,
+    "coins" : 3108500
+  }, {
+    "nickname" : "ShaAbi Khan",
+    "headimg" : "1507116857683.jpg",
+    "userid" : 2287913,
+    "coins" : 2584000
+  }, {
+    "nickname" : "Vicky.pardesi",
+    "headimg" : "1507127221359.jpg",
+    "userid" : 2268556,
+    "coins" : 2355500
+  }, {
+    "nickname" : "K H A N",
+    "headimg" : "1508195249958.jpg",
+    "userid" : 2279583,
+    "coins" : 1441000
+  }, {
+    "nickname" : "Shoaib_Jewllers \uD83D\uDC51",
+    "headimg" : "1507794504055.jpg",
+    "userid" : 2265144,
+    "coins" : 1391500
+  }, {
+    "nickname" : "\uD83D\uDC98DilJale\uD83D\uDCA5Pardesi\uD83C\uDF1F",
+    "headimg" : "1508010419016.jpg",
+    "userid" : 2052696,
+    "coins" : 1342000
+  }, {
+    "nickname" : "Ali Khan111",
+    "headimg" : "1508169240236.jpg",
+    "userid" : 2176074,
+    "coins" : 1336000
+  }, {
+    "nickname" : "\uD83D\uDC93\uD83D\uDC9B Mr Rafi \uD83D\uDC9B\uD83D\uDC93",
+    "headimg" : "1504226099124.jpg",
+    "userid" : 2140710,
+    "coins" : 1098500
+  }, {
+    "nickname" : "Shams Ud Din",
+    "headimg" : "1508179894958.jpg",
+    "userid" : 2237267,
+    "coins" : 956000
+  }, {
+    "nickname" : "silent  chiller",
+    "headimg" : "1507507910568.jpg",
+    "userid" : 2275857,
+    "coins" : 898000
+  } ]
+}
+
+
+
+
+
+var getImagePath = function(path, talentid) {
+    strtalentid = talentid + "";
+    head1 = strtalentid.substring(0, 3);
+    head2 = strtalentid.substring(3, 8);
+
+    if (path) {
+        var pa = path.split('.');
+        if (pa.length == 2) {
+            return 'http://s3-us-west-2.amazonaws.com/thankyo/image/7/0000/0' + head1 + "/" + head2 + "/" + pa[0] + '_80.' + pa[1];
+        }
+    }
+    return path;
+}
+//Coins Ranking
+// $.ajax({ 
+//     type: 'GET', 
+//     async:false,
+//     url: 'http://34.210.95.182:8080/db/search',
+//     crossDomain:true,
+//     data: { type:"gift_talent_eddievent_coins" }, 
+//     dataType: 'json',
+//     success: function (data) {  
+//       myObj2 =data;
+//       console.log(myObj);
+//     }
+// });
+
+
+//Gifter Ranking
+// $.ajax({ 
+//     type: 'GET', 
+//     async:false,
+//     url: 'http://34.210.95.182:8080/db/search',
+//     crossDomain:true,
+//     data: { type:"gift_user_eddievent_coins" }, 
+//     dataType: 'json',
+//     success: function (data) {  
+//       myObj3 =data;
+//       console.log(myObj2);
+//     }
+// });
+
+
+
+// Coins 
+
+
+var p;
+var q = 1;
+var container3 = document.getElementById("demo3");
+var res3;
+container3.innerHTML += '<div class="row top-heading  glow"><div class="col-sm-2 col-md-2 col-xs-2"> <img src="img/coins.png" class="img  my_img_top"></div><div class="col-sm-8 col-md-10 col-xs-10"> <div class="row row_merg"><p class="ranking-top"> <b style="color:black !important; ">COINS RANKING</b></p></div><div class="row row_merg"><p class="font_name top-users"> Top coins reciever from this month </p></div></div>' + '</div>';
+for (var p = 0; p < myObj2.data.length; p++) {
+    if (myObj2.data[p].nickname.length > 11) {
+        res3 = myObj2.data[p].nickname.substring(0, 11);
+        res3 = res3 + "...";
+    } else {
+        res3 = myObj2.data[p].nickname;
+    }
+    var headimg2 = getImagePath(myObj2.data[p].headimg, myObj2.data[p].targetid);
+    if(q==1) {
+      // container.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <img src="img/medal1.png" class="img  my_img1"></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="img/sample.jpg" class="img img-circle my_img"></div><div class="col-sm-6 col-md-8 col-xs-8"> <div class="row row_merg"><p class="font_name">' + res + '</p></div><div class="row row_bot"><div class="col-sm-1 col-md-1 col-xs-1"><img reward src="img/coin.png" class="img img_gift"></div><div class="col-sm-3 col-md-1 col-xs-2 reward">' + myObj.data[i].coins + '</div><div class="col-sm-1 col-md-1 col-xs-1"><img src="img/level icon 2.png" class="img img_gift"></div><div class="col-sm-3 col-md-1 col-xs-2 reward">' + myObj.data[i].number + '</div></div></div></div>' + '</div>';
+        container3.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <img src="img/medal1.png" class="img  my_img1"></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="'+headimg2+'" class="img img-circle my_img"></div><div class="col-sm-5 col-md-5 col-xs-5 toper_names">'+res3+'</div><div class="col-sm-3 col-md-3 col-xs-3 toper-rewards toper_names">' + myObj2.data[p].coins + '</div></div>' + '</div>';
+
+    }
+    if (q == 2) {
+         container3.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <img src="img/medal2.png" class="img  my_img1"></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="'+headimg2+'" class="img img-circle my_img"></div><div class="col-sm-5 col-md-5 col-xs-5 toper_names">'+res3+'</div><div class="col-sm-3 col-md-3 col-xs-3 toper-rewards toper_names">' + myObj2.data[p].coins + '</div></div>' + '</div>';
+    }
+
+    if (q == 3) {
+        container3.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <img src="img/medal3.png" class="img  my_img1"></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="'+headimg2+'" class="img img-circle my_img"></div><div class="col-sm-5 col-md-5 col-xs-5 toper_names">'+res3+'</div><div class="col-sm-3 col-md-3 col-xs-3 toper-rewards toper_names">' + myObj2.data[p].coins + '</div></div>' + '</div>';
+    }
+    if (q==4) {
+        container3.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <div class="numberCircle">' + q + '</div></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="'+headimg2+'" class="img img-circle my_img"></div><div class="col-sm-5 col-md-5 col-xs-5 toper_names">'+res3+'</div><div class="col-sm-3 col-md-3 col-xs-3 toper-rewards toper_names">' + myObj2.data[p].coins + '</div></div>' + '</div>';
+    }
+     if (q==5) {
+        container3.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <div class="numberCircle">' + q + '</div></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="'+headimg2+'" class="img img-circle my_img"></div><div class="col-sm-5 col-md-5 col-xs-5 toper_names">'+res3+'</div><div class="col-sm-3 col-md-3 col-xs-3 toper-rewards toper_names">' + myObj2.data[p].coins + '</div></div>' + '</div>';
+    }
+
+    q++;
+}
+var r=5;
+$("#show-more-top3").click(function(){
+    $("#show-more-top3").hide();
+    for (var p = 4; p < myObj2.data.length; p++) {
+    if (myObj2.data[p].nickname.length > 11) {
+        res3 = myObj2.data[p].nickname.substring(0, 12);
+        res3 = res3 + "...";
+    } else {
+        res3 = myObj2.data[p].nickname;
+    }
+    var headimg2 = getImagePath(myObj2.data[p].headimg, myObj2.data[p].targetid);
+     if (r>5) {
+        container3.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <div class="numberCircle">' + r + '</div></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="'+headimg2+'" class="img img-circle my_img"></div><div class="col-sm-5 col-md-5 col-xs-5 toper_names">'+res3+'</div><div class="col-sm-3 col-md-3 col-xs-3 toper-rewards toper_names">' + myObj2.data[p].coins + '</div></div>' + '</div>';
+    }
+
+    r++;
+}
+});
+
+
+// Top Fans
+
+
+var w;
+var x = 1;
+var container4 = document.getElementById("demo4");
+var res4;
+container4.innerHTML += '<div class="row top-heading  glow"><div class="col-sm-2 col-md-2 col-xs-2"> <img src="img/fans.png" class="img  my_img_top"></div><div class="col-sm-8 col-md-10 col-xs-10"> <div class="row row_merg"><p class="ranking-top"> <b style="color:black !important;">FANS RANKING</b></p></div><div class="row row_merg"><p class="font_name top-users"> Top coins reciever from this month </p></div></div>' + '</div>';
+for (var w = 0; w < myObj3.data.length; w++) {
+    if (myObj3.data[w].nickname.length > 12) {
+        res4 = myObj3.data[w].nickname.substring(0, 11);
+        res4 = res4 + "...";
+    } else {
+        res4 = myObj3.data[w].nickname;
+    }
+    var headimg2 = getImagePath(myObj3.data[w].headimg, myObj3.data[w].userid);
+    if(x==1) {
+      // container.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <img src="img/medal1.png" class="img  my_img1"></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="img/sample.jpg" class="img img-circle my_img"></div><div class="col-sm-6 col-md-8 col-xs-8"> <div class="row row_merg"><p class="font_name">' + res + '</p></div><div class="row row_bot"><div class="col-sm-1 col-md-1 col-xs-1"><img reward src="img/coin.png" class="img img_gift"></div><div class="col-sm-3 col-md-1 col-xs-2 reward">' + myObj.data[i].coins + '</div><div class="col-sm-1 col-md-1 col-xs-1"><img src="img/level icon 2.png" class="img img_gift"></div><div class="col-sm-3 col-md-1 col-xs-2 reward">' + myObj.data[i].number + '</div></div></div></div>' + '</div>';
+        container4.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <img src="img/medal1.png" class="img  my_img1"></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="'+headimg2+'" class="img img-circle my_img"></div><div class="col-sm-5 col-md-5 col-xs-5 toper_names">'+res4+'</div><div class="col-sm-3 col-md-3 col-xs-3 toper-rewards toper_names">' + myObj3.data[w].coins + '</div></div>' + '</div>';
+
+    }
+    if (x == 2) {
+         container4.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <img src="img/medal2.png" class="img  my_img1"></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="'+headimg2+'" class="img img-circle my_img"></div><div class="col-sm-5 col-md-5 col-xs-5 toper_names">'+res4+'</div><div class="col-sm-3 col-md-3 col-xs-3 toper-rewards toper_names">' + myObj3.data[w].coins + '</div></div>' + '</div>';
+    }
+
+    if (x == 3) {
+        container4.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <img src="img/medal3.png" class="img  my_img1"></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="'+headimg2+'" class="img img-circle my_img"></div><div class="col-sm-5 col-md-5 col-xs-5 toper_names">'+res4+'</div><div class="col-sm-3 col-md-3 col-xs-3 toper-rewards toper_names">' + myObj3.data[w].coins + '</div></div>' + '</div>';
+    }
+    if (x==4) {
+        container4.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <div class="numberCircle">' + x + '</div></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="'+headimg2+'" class="img img-circle my_img"></div><div class="col-sm-5 col-md-5 col-xs-5 toper_names">'+res4+'</div><div class="col-sm-3 col-md-3 col-xs-3 toper-rewards toper_names">' + myObj3.data[w].coins + '</div></div>' + '</div>';
+    }
+     if (x==5) {
+        container4.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <div class="numberCircle">' + x + '</div></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="'+headimg2+'" class="img img-circle my_img"></div><div class="col-sm-5 col-md-5 col-xs-5 toper_names">'+res4+'</div><div class="col-sm-3 col-md-3 col-xs-3 toper-rewards toper_names">' + myObj3.data[w].coins + '</div></div>' + '</div>';
+    }
+
+    x++;
+}
+var y=5;
+$("#show-more-top4").click(function(){
+    $("#show-more-top4").hide();
+    for (var p = 4; p < myObj3.data.length; p++) {
+    if (myObj3.data[p].nickname.length > 11) {
+        res4 = myObj3.data[p].nickname.substring(0, 12);
+        res4 = res3 + "...";
+    } else {
+        res4 = myObj3.data[p].nickname;
+    }
+    var headimg2 = getImagePath(myObj3.data[p].headimg, myObj3.data[p].userid);
+     if (y>5) {
+        container4.innerHTML += '<div class="row draw-line"><div class="col-sm-2 col-md-2 col-xs-2"> <div class="numberCircle">' + y + '</div></div><div class="col-sm-2 col-md-2 col-xs-2"> <img src="'+headimg2+'" class="img img-circle my_img"></div><div class="col-sm-5 col-md-5 col-xs-5 toper_names">'+res4+'</div><div class="col-sm-3 col-md-3 col-xs-3 toper-rewards toper_names">' + myObj3.data[p].coins + '</div></div>' + '</div>';
+    }
+
+    y++;
+}
+});
